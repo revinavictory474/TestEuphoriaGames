25c9a2d5293c64873c259918a3e4d3ae51d205a7
Revina Victory <revina.victory@gmail.com>
2021-09-30 13:31:40 +0700

    TODO refactoring,  ui score add

--- TestEuphoria/Assets/Code/Player/PlayerPut.cs
+++ TestEuphoria/Assets/Code/Player/PlayerPut.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPut : MonoBehaviour
 {
+    [SerializeField] private Text cubeTxt;
+    [SerializeField] private Text sphereTxt;
+    [SerializeField] private Text capsuleTxt;
+    //[SerializeField] internal GameObject bgScore;
+
+    internal int scoreCube;
+    internal int scoreSphere;
+    internal int scoreCapsule;
+
+    internal Text[] textScore;
     internal Test test;
     internal GameObject targetPut;
 
@@ -14,6 +25,7 @@ public class PlayerPut : MonoBehaviour
     private void Start()
     {
         playerTake = GetComponent<PlayerTake>();
+        //textScore = bgScore.GetComponentsInChildren<Text>();
     }
 
     private void Update()
@@ -35,11 +47,12 @@ public class PlayerPut : MonoBehaviour
                 targetPut = hit.collider.gameObject;
                 test = targetPut.GetComponent<Test>();
                 id = test.figure.GetHashCode();
+
                 if (id == playerTake.idObj)
                 {
                     playerTake.targetPickUp.gameObject.SetActive(false);
+                    TextScore(id);
                     Debug.Log("test");
-
                 }
 
             }
@@ -47,6 +60,28 @@ public class PlayerPut : MonoBehaviour
         else
         {
             playerTake.popUp.SetActive(false);
+        }
+    }
+
+    private void TextScore(int id)
+    {
+        switch(id)
+        {
+            case 0:
+                scoreCube++;
+                cubeTxt.text = scoreCube.ToString();
+                break;
+
+            case 1:
+                scoreSphere++;
+                sphereTxt.text = scoreSphere.ToString();
+                break;
+
+            case 2:
+                scoreCapsule++;
+                capsuleTxt.text = scoreCapsule.ToString();
+                break;
+
         }
     }
 }
